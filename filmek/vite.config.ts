21c8467d1d6c2d266e49d79962247d5290c46f66
molnarbea <molnarbeatrix11@gmail.com>
2026-09-24 22:56:56 +0200

    pages link

--- filmek/vite.config.ts
+++ filmek/vite.config.ts
@@ -4,8 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+    base: '/film_react/', 
 })
